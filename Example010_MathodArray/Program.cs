2bff75acfeb34ccb743de8c60a5a2f51b8000767
hrustalevanv <hrustalevanv2021@gmail.com>
2021-12-04 16:05:08 +0300

    ArrayLibrary

--- Example010_MathodArray/Program.cs
+++ Example010_MathodArray/Program.cs
@@ -1,4 +1,5 @@
-﻿int[] array = { 1, 12, 31, 4,18, 15, 16, 17, 18 };
+﻿int[] array = {1,12,31,4,15,18,16,17,18};//если два и более одинаковые значения, 
+//то в выходе покажет консоль их все, в таком случае используем break, 
 
 int n = array.Length;
 int find = 18;
@@ -7,12 +8,11 @@ int index = 0;
 
 while (index < n)
 {
-
     if(array[index] == find)
     {
         Console.WriteLine(index);
-        break;
+        break; //  если выполняется условие, значит стоп
     }
-    //index = index + 1;
     index++;
 }
+
--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,52 +1,60 @@
-﻿void FillArray(int[] collection)
+﻿/* Перепишем данную задачу с помощью методов и генератором псевдо случайных чисел
+1. возьмем метод, передать в него массив и заполнить массив необходимым количеством элементов
+2. Описание метода, который будет выводить все элементы по порядку
+3. Превратим код поиска нужного нам индекса в метод
+*/
+
+void FillArray(int[] collection)//метод void, далее даем наименование методу FillArray,
+//в качестве аргумента int[] collection
 { 
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
+    int length = collection.Length;//длинна массива collection.Length
+    int index = 0;// создаем переменную
+    while (index < length)//запускаем цикл
     {
-        collection[index] = new Random().Next(1, 10);
+        collection[index] = new Random().Next(1, 10);//обращаемся к аргументу и 
+        //кладем новое случайное целое число new Random из диапазона 1,10 
         index = index + 1;
     }
 
 
 }
 
-void PrintArray(int[] col)
+void PrintArray(int[] col)// второй метод, который будет печатать массив
 {
-    int count = col.Length;
-    int position = 0;
+    int count = col.Length;//количество элементов
+    int position = 0;//создаем переменную
     while (position < count)
     {
-        Console.WriteLine(col[position]);
+        Console.WriteLine(col[position]);//выводим значение текущего элемента
         position++;
     }
 }
-
+// находим позицию нужного нам элемента в массиве с использованием метода
 int IndexOf(int[] collection, int find)
 {
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
+    int count = collection.Length;//определяем количество элементов count
+    int index = 0;//создаем переменную
+    int position = -1;//создаем переменную
     while (index < count)
     {
-        if(collection[index] == find)
+        if(collection[index] == find)// == совпал
         {
-            position = index;
+            position = index;//тогда нужно сохранить позицию в position кладем значение index
             break;
         }
         index++;  
     }
-    return position;
+    return position;//ожидаем нашу позицию элемента
 }
 
 int[] array = new int[10];
 
-FillArray(array);
+FillArray(array);//наименование нашего массива
 array[4] = 4;
 array[6] = 4;
 
-PrintArray(array);
+PrintArray(array);//распечатывает наш массив
 Console.WriteLine();
 
-int pos = IndexOf(array, 444);
+int pos = IndexOf(array, 444);//определили переменную и положили ва нее IndexOf
 Console.WriteLine(pos);
