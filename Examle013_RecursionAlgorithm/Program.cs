2dfffe2a377328034557530a656f4e320de303c8
hrustalevanv <hrustalevanv2021@gmail.com>
2021-12-08 14:25:49 +0300

    Examle013_Recursion Algorithm

--- Examle013_RecursionAlgorithm/Program.cs
+++ Examle013_RecursionAlgorithm/Program.cs
@@ -19,35 +19,73 @@
 
 //описываем метод, который будет двухмерную матрицу печатать на экран  
 
-void PrintArray(int[,] matr)
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     //for (int i = 0; i < 3; i++) //указываем новый функционал GetLength (количество строк)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         //for (int j = 0; j < 4; j++)
+//         {
+//             Console.Write($"{matr[i, j]} ");
+//         }
+//         Console.WriteLine();   
+//     }
+// }
+
+// //описываем дополнительный метод, который будет 
+// //заполнять нашу матрицу случайными числами
+// void FillArray(int[,] matr)
+// {
+//      for (int i = 0; i < matr.GetLength(0); i++)
+//      {
+//           for (int j = 0; j < matr.GetLength(1); j++)
+//           {
+//               matr[i,j] = new Random().Next(1,10);//[1,10)
+//           }
+//      }
+// }
+
+// int[,] matrix = new int[3, 4];
+// PrintArray(matrix);
+// FillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
+
+
+//Факториал. Определить метод, который будет принимать число,
+// факториал которого нужно вычеслить
+// double Factorial (int n)
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if(n == 1) return 1; //если мы дошли до ед, т.е. N == 1, то мы возвращаем ед.-определение факториала.
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// //Console.WriteLine(Factorial(3)); //1*2*3 = 6
+
+
+
+// Числа Фибоначчи, где каждое слуд число задается предыдущим.
+//Теперь попробуем записать кодом вывод этих чисел.
+// f(1) = 1
+// f(2) = 1
+//f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
-    //for (int i = 0; i < 3; i++) //указываем новый функционал GetLength (количество строк)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        //for (int j = 0; j < 4; j++)
-        {
-            Console.Write($"{matr[i, j]} ");
-        }
-        Console.WriteLine();   
-    }
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
 }
 
-//описываем дополнительный метод, который будет 
-//заполнять нашу матрицу случайными числами
-void FillArray(int[,] matr)
+for (int i = 1; i < 50; i++)
 {
-     for (int i = 0; i < matr.GetLength(0); i++)
-     {
-          for (int j = 0; j < matr.GetLength(1); j++)
-          {
-              matr[i,j] = new Random().Next(1,10);//[1,10)
-          }
-     }
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
 }
-
-int[,] matrix = new int[3, 4];
-PrintArray(matrix);
-FillArray(matrix);
-Console.WriteLine();
-PrintArray(matrix);
